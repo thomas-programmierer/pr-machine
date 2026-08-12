7fc3094f0b2d9e922b9269380754a3383e945b0a
Tom Mochalski <thomas.peter.mochalski@gmail.com>
2026-08-12 15:01:49 +0200

    fix: PATCH zu CORS Allow-Methods hinzugefügt

--- server.js
+++ server.js
@@ -172,7 +172,7 @@ function parseMultipart(buffer, boundary) {
 const server = http.createServer(async (req, res) => {
   if (req.method === "OPTIONS") {
     res.writeHead(204, { "Access-Control-Allow-Origin":"*",
-      "Access-Control-Allow-Methods":"GET,POST,PUT,DELETE,OPTIONS",
+      "Access-Control-Allow-Methods":"GET,POST,PUT,PATCH,DELETE,OPTIONS",
       "Access-Control-Allow-Headers":"Content-Type,x-hub-token" });
     return res.end();
   }
